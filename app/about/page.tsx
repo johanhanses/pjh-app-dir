cee941658bd7fe5110795523a0d4a709be3f181a
Johan Hansdes <johanhanses@gmail.com>
2022-12-11 21:06:17 +0100

    More todos

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,6 +5,10 @@ export default function page() {
       <ul className="space-y-2 md:space-y-1">
         <li className="list-disc list-inside line-through">Fix Lighthouse scores</li>
         <li className="list-disc list-inside">Add colorMode, check compat with next-themes and nextjs appDir</li>
+        <li className="list-disc list-inside">Add active classes to desktop nav links</li>
+        <li className="list-disc list-inside">Next Auth?</li>
+        <li className="list-disc list-inside">Sanity? for posts?</li>
+        <li className="list-disc list-inside">Migrate CV.</li>
         <li className="list-disc list-inside line-through">Mobile menu</li>
         <li className="list-disc list-inside line-through">Animate mobile menu</li>
         <li className="list-disc list-inside line-through">Close mobile menu on internal navigation</li>
