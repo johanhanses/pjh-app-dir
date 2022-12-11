c081f63fa5842cb3bbc2e2e984b238a320042693
Johan Hansdes <johanhanses@gmail.com>
2022-12-11 17:03:57 +0100

    line-through

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,7 @@ export default function page() {
     <div>
       <h2 className="font-notoSerifDisplay uppercase mb-4">Todo</h2>
       <ul className="space-y-2 md:space-y-1">
-        <li className="list-disc list-inside">Fix Lighthouse scores</li>
+        <li className="list-disc list-inside line-through">Fix Lighthouse scores</li>
         <li className="list-disc list-inside">Add colorMode, check compat with next-themes and nextjs appDir</li>
         <li className="list-disc list-inside line-through">Mobile menu</li>
         <li className="list-disc list-inside line-through">Animate mobile menu</li>
